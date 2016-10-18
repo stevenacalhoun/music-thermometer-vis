cdbadeb486f0b52658d5998fa8b5267981e032da
stevenacalhoun <stevenacalhoun@gmail.com>
2016-10-17 22:23:46 -0400

    Data aggregation done in separate function. Better tooltips

--- app/scripts/streamGraph.js
+++ app/scripts/streamGraph.js
@@ -1,4 +1,6 @@
-dr = require('d3');
+var d3 = require('d3'),
+    tip = require('d3-tip'),
+    constants = require('./constants.js');
 
 module.exports = {
   showData: function(data) {
@@ -7,67 +9,53 @@ module.exports = {
       container.append("<span>"+d.key+","+d.value+","+d.date+"</span>");
     })
   },
-  streamGraph: function(data) {
+  streamGraph: function(rawData) {
+    // Prep data
+    var preppedData = aggregateData(rawData),
+        data = preppedData.data,
+        artists = preppedData.artists;
+
     $('body').append("<div class='chart'></div>");
 
-    var artists = [];
-    var dataObject = {};
-    data.forEach(function(d){
-      // Keep up with all artists
-      if ($.inArray(d.key, artists) == -1) {
-        artists.push(d.key);
-      }
-
-      // Add new entry for date
-      if ((d.date in dataObject) == false) {
-        dataObject[d.date] = {
-          "date": d.date
-        };
-      }
-
-      dataObject[d.date][d.key] = +d.value;
-    });
-
-    // Add artists that aren't in there yet
-    Object.keys(dataObject).forEach(function(date) {
-      artists.forEach(function(artist){
-        if ((artist in dataObject[date]) == false) {
-          dataObject[date][artist] = 0;
-        }
-      })
-    })
-
-    // Flatten dictionary
-    var dataObjectList = $.map(dataObject, function(d){ return d; });
-
-    var colorrange = ["#045A8D", "#2B8CBE", "#74A9CF", "#A6BDDB", "#D0D1E6", "#F1EEF6"];
-    var strokecolor = colorrange[0];
-    var datearray = [];
-
+    // Sizing
     var margin = {top: 20, right: 40, bottom: 30, left: 30};
     var width = document.body.clientWidth - margin.left - margin.right;
     var height = 400 - margin.top - margin.bottom;
 
-    var tooltip = d3.select("body")
-        .append("div")
-        .attr("class", "remove")
-        .style("position", "absolute")
-        .style("z-index", "20")
-        .style("visibility", "hidden")
-        .style("top", "30px")
-        .style("left", "55px");
+    // Stack
+    var stack = d3.stack()
+        .keys(artists)
+        .offset(d3.stackOffsetSilhouette);
 
+    // Create layered data from stack
+    var layersData = stack(data);
+
+    // Scales: x, y, color
     var x = d3.scaleTime()
         .range([0, width])
+        .domain(d3.extent(data, function(d) { return new Date(d.key); }));
 
     var y = d3.scaleLinear()
-        .range([height-10, 0]);
+        .range([height-10, 0])
+        .domain([
+          d3.min(layersData, function(layer) {
+            return d3.min(layer, function(d) {
+              return d[1];
+            })
+          }),
+          d3.max(layersData, function(layer) {
+            return d3.max(layer, function(d) {
+              return d[1];
+            })
+          })
+        ]);
 
-    var z = d3.scaleOrdinal()
-        .range(colorrange);
+    var color = d3.scaleOrdinal()
+        .range(constants.colors);
 
+    // Axes
     var xAxis = d3.axisBottom()
-        .scale(x)
+        .scale(x);
 
     var yAxis = d3.axisLeft()
         .scale(y);
@@ -75,36 +63,41 @@ module.exports = {
     var yAxisr = d3.axisRight()
         .scale(y);
 
-    var stack = d3.stack()
-        .keys(artists)
-
-    x.domain(d3.extent(data, function(d) { return new Date(d.date); }));
-
+    // Area function
     var area = d3.area()
         .x(function(d) {
-            dateObj = new Date(d.data.date);
-            return x(dateObj);
-         })
-        .y0(function(d) { return y(d[0]); })
-        .y1(function(d) { return y(d[1]); });
+          dateObj = new Date(d.data.key);
+          return x(dateObj);
+        })
+        .y0(function(d) {
+          return y(d[0]);
+        })
+        .y1(function(d) {
+          return y(d[1]);
+        });
 
+    // Hover tooltip
+    var tooltip = tip()
+        .attr('class', 'd3-tip')
+        .html(function(d) {
+          // Need some better tooltip html
+          return d;
+        })
+        .offset([-10,0]);
+
+    // Main container
     var svg = d3.select(".chart").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
+        .call(tooltip)
       .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var layersData = stack(dataObjectList);
+    // Hidden circle for tooltip on mouse
+    var tipCircle = svg.append("circle")
+      .attr('id', 'tip-circle');
 
-    y.domain([0, d3.max(layersData[layersData.length-1], function(d) { return d[1]; })]);
-
-    svg.selectAll(".layer")
-        .data(layersData)
-      .enter().append("path")
-        .attr("class", "layer")
-        .attr("d", area)
-        .style("fill", function(d, i) { return z(i); });
-
+    // Axes render
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
@@ -119,66 +112,156 @@ module.exports = {
         .attr("class", "y axis")
         .call(yAxis);
 
+    // Render layers from layer data with area function
     svg.selectAll(".layer")
-      .attr("opacity", 1)
+        .data(layersData)
+      .enter().append("path")
+        .attr("class", "layer")
+        .attr("d", area)
+        .attr("opacity", 1)
+        .style("fill", function(d, i) { return color(i); });
+
+    // Mouseover functions
+    svg.selectAll(".layer")
       .on("mouseover", function(d, i) {
+        // Lower opacity of all layers but hovered 1
         svg.selectAll(".layer").transition()
-        .duration(250)
-        .attr("opacity", function(d, j) {
-          return j != i ? 0.6 : 1;
-      })})
+          .duration(250)
+          .attr("opacity", function(d, j) {
+            return j != i ? 0.2 : 1;
+        })
+      })
 
       .on("mousemove", function(d, i) {
-        mousex = d3.mouse(this);
-        mousex = mousex[0];
-        var invertedx = x.invert(mousex);
-        invertedx = invertedx.getMonth() + invertedx.getDate();
+        // Find correct info
+        var mouseDate = x.invert(d3.mouse(this)[0]);
 
+        // Create date array (should be a better way)
+        datearray = [];
         for (var k = 0; k < d.length; k++) {
-          datearray[k] = new Date(d[k].data.date);
-          datearray[k] = datearray[k].getMonth() + datearray[k].getDate();
+          date = new Date(d[k].data.key);
+          datearray[k] = date.getFullYear() + "/" + date.getMonth();
         }
 
-        mousedate = datearray.indexOf(invertedx);
-        pro = d[mousedate][0];
+        // Get value at date
+        mouseDateIndex = datearray.indexOf(mouseDate.getFullYear() + "/" + mouseDate.getMonth());
+        value = d[mouseDateIndex][1] - d[mouseDateIndex][0];
 
-        d3.select(this)
-        .classed("hover", true)
-        .attr("stroke", strokecolor)
-        .attr("stroke-width", "0.5px"),
-        tooltip.html( "<p>" + d.key + "<br>" + pro + "</p>" ).style("visibility", "visible");
+        // Move circle to mouse
+        tipCircle.attr('cx', d3.mouse(this)[0])
+          .attr('cy', d3.mouse(this)[1]);
+
+        // Render tooltip on circle
+        tooltip.show(value, tipCircle.node());
       })
 
       .on("mouseout", function(d, i) {
-       svg.selectAll(".layer")
-        .transition()
-        .duration(250)
-        .attr("opacity", "1");
-        d3.select(this)
-        .classed("hover", false)
-        .attr("stroke-width", "0px"), tooltip.html( "<p>" + d.key + "<br>" + pro + "</p>" ).style("visibility", "hidden");
+        // Hide tooltip
+        tooltip.hide(d);
+
+        // All layers back to full opacity
+        svg.selectAll(".layer")
+          .transition()
+          .duration(250)
+          .attr("opacity", "1");
       })
 
+    // Vertical bar vis
     var vertical = d3.select(".chart")
-          .append("div")
-          .attr("class", "remove")
-          .style("position", "absolute")
-          .style("z-index", "19")
-          .style("width", "1px")
-          .style("height", "380px")
-          .style("top", "10px")
-          .style("bottom", "30px")
-          .style("left", "0px")
-          .style("background", "#fff");
+        .append("div")
+        .attr("class", "remove")
+        .style("position", "absolute")
+        .style("z-index", "19")
+        .style("width", "1px")
+        .style("height", "380px")
+        .style("top", "10px")
+        .style("bottom", "30px")
+        .style("left", "0px")
+        .style("background", "#fff");
 
+    // Move vertical bar
     d3.select(".chart")
-        .on("mousemove", function(){
-           mousex = d3.mouse(this);
-           mousex = mousex[0] + 5;
-           vertical.style("left", mousex + "px" )})
-        .on("mouseover", function(){
-           mousex = d3.mouse(this);
-           mousex = mousex[0] + 5;
-           vertical.style("left", mousex + "px")});
+      .on("mousemove", function() {
+        vertical.style("left", (d3.mouse(this)[0] + 5) + "px" )
+      })
    }
+}
+
+function aggregateData(data) {
+  // Aggregate weeks into months
+  var artists = [];
+  var i = 0;
+  data.forEach(function(d){
+    // Keep up with all artists
+    if ($.inArray(d.artist, artists) == -1) {
+      artists.push(d.artist);
+    }
+  })
+
+  var aggregateData = d3.nest()
+    .key(function(d) {
+      // month/year key
+      var dateObj = new Date(d.chartWeek);
+      var dateKey = new Date(dateObj.getFullYear(), dateObj.getMonth()-1, 1);
+      return dateKey;
+    })
+    .rollup(function(leaves) {
+      // Combine all artist data into one long list
+      artistData = [];
+      leaves.forEach(function(leaf) {
+        artistData.push(leaf.artist);
+      });
+
+      // Reduce artist data
+      var nestedLeaves = d3.nest()
+        .key(function(d) {
+          return d
+        })
+        .rollup(function(leaves) {
+          return leaves.length
+        })
+        .entries(artistData)
+
+      var artistDict = {};
+      nestedLeaves.forEach(function(d) {
+        artistDict[d.key] = d.value;
+      });
+
+      return artistDict
+    })
+    .entries(data)
+
+  var aggregateDatanew = [];
+  aggregateData.forEach(function(d) {
+    var dict = {
+      "key": d.key
+    }
+    $.extend(dict, d.value);
+
+    aggregateDatanew.push(dict);
+  });
+
+  // Sort by date
+  aggregateDatanew.sort(function(a,b) {
+    if (new Date(a.key) < new Date(b.key)) {
+      return -1;
+    }
+    else {
+      return 1;
+    }
+  })
+
+  // Add artists that aren't in data yet as 0
+  aggregateDatanew.forEach(function(dateEntry) {
+    artists.forEach(function(artist){
+      if ((artist in dateEntry) == false) {
+        dateEntry[artist] = 0;
+      }
+    })
+  })
+
+  return {
+    "data": aggregateDatanew,
+    "artists": artists
+  };
 }
