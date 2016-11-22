f60deddcef4005190dbf3a7816e891509010834e
stevenacalhoun <stevenacalhoun@gmail.com>
2016-11-22 16:11:36 -0600

    Changed dates back to 2009
    Really just a test for a setup thing

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
   colors: ["#a6cee3", "#1f78b4", "#b2df8a", "#33a02c", "#fb9a99", "#e31a1c", "#fdbf6f", "#ff7f00", "#cab2d6", "#6a3d9a", "#ffff99", "#b15928"],
   streamColors1: [ "#d0d1e6", "#a6bddb", "#74a9cf", "#3690c0", "#0570b0", "#045a8d", "#023858"],
-  streamColors2: ["#fdd49e","#fdbb84","#fc8d59","#ef6548","#d7301f","#b30000","#7f0000"],
+  streamColors2: ["#fdd49e","#7f0000","#fdbb84","#fc8d59","#ef6548","#d7301f","#b30000",],
   strokeocolor: "#000000"
 }
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,13 +9,13 @@ require('../styles/main.scss');
 
 // Get data for a range then pass this data to a stream graph
 var dateRange = {
-  "startDate": new Date(2000,0,1),
-  "endDate": new Date(2001,11,31),
+  "startDate": new Date(2009,0,1),
+  "endDate": new Date(2009,11,31),
 }
 
 apiCalls.getChartRangeCountry('both', dateRange, function(data) {
   streamGraph.streamGraph(data, 'body');
-}, 10);
+}, 100);
 
 // apiCalls.getChartRangeCountry('uk', dateRange, function(data) {
 //   streamGraph.streamGraph(data, 'body', 'uk-streamGraph');
