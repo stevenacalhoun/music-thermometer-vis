93f8fc6d0d77fa5582c1ddfa5255777502bef359
stevenacalhoun <stevenacalhoun@gmail.com>
2016-12-04 20:32:49 -0500

    Fix for easy rebase

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,16 @@
 var apiCalls = require('./apiCalls.js'),
     secrets = require('./secrets.js'),
-    streamGraph = require('./streamGraph.js'),
     constants = require('./constants.js'),
+    streamGraph = require('./streamGraph.js')
+    barGraph = require('./barGraph.js')
     d3 = require('d3'),
     $ = require('jQuery');
 
 require('../styles/main.scss');
+
+startDate = new Date(1992,1,1);
+endDate = new Date(1993,12,31);
+
 
 // Get data for a range then pass this data to a stream graph
 var dateRange = {
