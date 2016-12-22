c5b5a610241a22810513c9dd1fadffa6858cb671
stevenacalhoun <stevenacalhoun@gmail.com>
2016-12-22 13:03:48 -0600

    Change to server address, add splash screen

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,10 +6,5 @@ var apiCalls = require('./apiCalls.js'),
 require('../styles/main.scss');
 require('../styles/genericons/genericons.css');
 
-// introScreen.createLandingScreen();
-
-introScreen.createVisScreen($("<div></div>"));
-//
-// setTimeout(function() {
-//   // $('#Kelly_Clarkson_uk').d3Click()
-// }, 2000)
+introScreen.createLandingScreen();
+// introScreen.createVisScreen($("<div></div>"));
